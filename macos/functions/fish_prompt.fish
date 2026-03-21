a6b10fda9531965860ac934f9724068632844ea5
vxJ <dirtjumper@gmx.ch>
2026-03-21 19:34:35 +0100

    Add mac prompt config

---
+++ macos/functions/fish_prompt.fish
@@ -0,0 +1,9 @@
+function fish_prompt
+    set_color 87CEEB # Light blue
+    echo -n '居瑞斯 '
+    set_color 666666 # Gray for directory
+    echo -n (prompt_pwd)
+    set_color 87CEEB # Light blue
+    echo -n ' $ '
+    set_color normal
+end
